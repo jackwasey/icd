c39e24095082e29d341b6ecef6f9a8994006bee2
Jack Wasey <jack@jackwasey.com>
2014-06-01 22:12:27 -0400

    including SAS parsing code

---
+++ R/sas.R
@@ -0,0 +1,130 @@
+#' @title extract assignments from a SAS FORMAT definition.
+#' @description this is modelled entirely on a single chunk of SAS code, but
+#'   hopefully will have some generalizability. It relies heavily on lists and
+#'   regex, but, as you will see from the code, R is not a great language with
+#'   which to write a SAS parser.
+#'   
+#' ##examples
+#' #\dontrun{
+#' #sasFormatExtract(readLines('inst/extdata//comformat2012-2013.txt'))
+#' #}
+#' @param sasTxt is a character vector, with one item per line, e.g. from
+#'   \code{readLines}
+#' @import futile.logger
+#' @references
+#'   \url{http://support.sas.com/documentation/cdl/en/proc/61895/HTML/default/viewer.htm#a002473474.htm}
+#'    \url{https://communities.sas.com/thread/47571?start=0&tstart=0} 
+#'   \url{https://communities.sas.com/message/165945}
+#' @export
+sasFormatExtract <- function(sasTxt) {
+  
+  # drop white space (before adding new lines...)
+  #sasTxtNoWhite <- gsub(pattern="[[:space:]]", "", sasTxt)
+  
+  # collapse everything onto one big line, so we can filter multi-line
+  # commments. No ability to do multiline regex along a vector.
+  sasTxt <- paste(sasTxt, collapse=" \\n")
+  
+  # sas comments are in the form /* ... */ inline/multiline, or * ... ;
+  sasTxt <- gsub(pattern="/\\*.*?\\*/", replacement="", x=sasTxt)
+  sasTxt <- gsub(pattern="\\n\\*.*?;", replacement="\\n", x=sasTxt)
+  
+  #
+  sasTxt <- strsplit(sasTxt, split="\\;")[[1]]
+  #sasCleanLines <- strsplit(sasNoComments, split="\\\\n")[[1]]
+  #sasNonEmptyLines <- sasCleanLines[sasCleanLines!=""]
+  
+  #strip whitespace and ?undetected newline characters, replace with single spaces.
+  sasTxt <- gsub(pattern="\\\\n", "", sasTxt)
+  sasTxt <- gsub(pattern="[[:space:]]+", " ", sasTxt)
+  sasTxt <- trim(sasTxt)
+  
+  # drop everything except VALUE statements
+  sasTxt <- grep(pattern="^VALUE.*", x=sasTxt, value=T)
+  
+  # put each VALUE declaration in a vector element
+  allAssignments <- strMultiMatch(
+    pattern = "^VALUE[[:space:]]+([[:graph:]]+)[[:space:]]+(.+)[[:space:]]*$",
+    text=sasTxt)
+  
+  out <- list()
+  
+  #aNames <- vapply(allAssignments, '[[', 1, FUN.VALUE=rep("", length(allAssignments)))
+  #aVals <- vapply(allAssignments, '[[', 2, FUN.VALUE=rep("", length(allAssignments)))
+  
+  for (m in allAssignments) {
+    flog.debug("working on assignment pair:", m, capture=T)
+    out[m[[1]]] <- list(sasParseAssignments(m[[2]]))
+  }
+  
+  out
+}
+
+#' @title get assignments from a character string, or vector of character 
+#'   strings.
+#' @description   #form is aaa-bbb, ccc-ddd, eee, etc. = "name" abc-def, ghi,
+#'   etc. = "anothername" there is no delimiter between each assignment. '
+#'   @import futile.logger
+#' @return list with each list item containing a matrix of "char ranges",
+#'   "assigned value" pairs
+sasParseAssignments <- function(x, stripWhiteSpace=T, stripQuotes=T) {
+  
+  stopifnot(length(x)==1) # sorry...
+  # splitting with clever regex to separate each pair of assignments seems
+  # tricky, so doing it in steps.
+  halfway <- as.list(unlist(
+    strsplit(x, split="[[:space:]]*=[[:space:]]*") # n.b. this is a list with list per input row.
+  ))
+  
+  # we need to match the first unquoted space to get the boundary between the
+  # previous definition and the next variable name
+  if (length(halfway) == 2) { 
+    # we have just a single name value pair so just set name to value and return list of one item.
+    if (stripWhiteSpace) halfway <- gsub(pattern="[[:space:]]*", replacement="", halfway)
+    if (stripQuotes) halfway <- gsub(pattern='"', replacement="", halfway)
+    out <- list()
+    out[[halfway[[2]]]] <- unlist(strsplit(x=halfway[[1]], split=","))
+    return(out)
+  }
+  
+  threequarters <- c(
+    halfway[[1]],
+    unlist(
+      strMultiMatch(
+        pattern='^([^"]|"[^"]*")*? (.*)',
+        text = halfway[seq(2,length(halfway)-1)]
+      )
+    ),
+    halfway[[length(halfway)]]
+  )
+  
+  if (stripQuotes) threequarters <- gsub(pattern='"', replacement="", threequarters)
+  
+  #spaces may matter still, so don't randomly strip them?
+  
+  
+  flog.debug("length of threequarters = %d", length(threequarters))
+  
+  out <- list()
+  for (pair in seq(from=1, to=length(threequarters), by=2)) {
+    flog.debug("%s = %s", threequarters[pair], threequarters[pair+1])
+    if (stripWhiteSpace) {
+      outwhite <- gsub(pattern="[[:space:]]*", replacement="", threequarters[pair])
+    } else {
+      outwhite <- threequarters[pair]
+    }
+    out[[threequarters[pair+1]]] <- unlist(strsplit(x=outwhite, split=","))
+  }
+  out
+}
+
+#' @title drop superfluous assignment name when the name is already defined.
+#' @description this is happening in the DRG definitions for AHRQ comorbidities. We have data like:
+#' "HTNDRG" 079,305 = "YES"
+#' I would like this to be list(HTNDRG="079,305")
+#' @param x in this case is '079,305 = "YES"' (quotes may be present in the string itself)
+#' @keywords internal
+sasDropOtherAssignment <- function(x) {
+  #regmatches(m=regexec(pattern="(.*)=", text='079,305 = "YES"'), x='079,305 = "YES"')[[1]][2]
+  lapply(x, function(y) strsplit(y, split="[[:space:]]*=")[[1]][1]) # asssuming one "="
+}
---
+++ tests/testthat/test-sas.R
@@ -0,0 +1,62 @@
+
+test_that("basic SAS VALUE declarations can be read into an R list", {
+  
+  # general format from my n=1 sample seems to be:
+  # VALUE somename numbers, "ranges"-"ranges", ranges-ranges = "mappinname"
+  
+  
+  result <- sasFormatExtract('VALUE JACK 12345 = "TESTTHAT"')
+  expect_equal(result, list(JACK = list(TESTTHAT = "12345")))
+  
+  result <- sasFormatExtract('VALUE JACK 12345, 22222 = "TESTTHAT"')
+  expect_equal(result, list(JACK = list(TESTTHAT = c("12345","22222"))))
+  
+  result <- sasFormatExtract('VALUE TESTTHAT 100-102 = "YES"')
+  expect_equal(result, list(TESTTHAT = list("YES" = "100-102")))
+  
+  
+  result <- sasFormatExtract('VALUE $RFRMSOMETHING 100-102 = "HTN" 103,104 = "CHF"')
+  expect_equal(result, list("$RFRMSOMETHING" = 
+                              list(HTN = "100-102", CHF=c("103","104")))
+  )
+  
+  
+  result <- sasFormatExtract(c('VALUE $RFRMSOMETHING 100-102 = "HTN" 103,104 = "CHF";\n',
+                               ' VALUE otherstuff 105 = "YES"'))
+  expect_equal(result, list("$RFRMSOMETHING" = list(HTN = "100-102", CHF=c("103","104")),
+                            otherstuff = list("YES" = '105')))
+  
+  result <- sasFormatExtract(c('   VALUE $RFRMSOMETHING 100-102 = "HTN" 103,104 = "CHF";',
+                               'VALUE  otherstuff   105   =  "  Y E S " '))
+  expect_equal(result, list("$RFRMSOMETHING" = list(HTN = "100-102", CHF=c("103","104")),
+                            otherstuff = list("YES" = '105')))
+  
+  # make sure it tolerates lack of whitespace...
+  
+  
+  #print(getwd()) # we're in the testthat directory, at least when running this code with test_file
+  #sasTxt <- readLines('inst/extdata//comformat2012-2013.txt')
+  
+})
+
+test_that("proc format opening is understood", {})
+
+test_that("groups of SAS assignments can be extracted", {
+  
+  # this is a single assignment, but of the form of sub-part of multi-assignment, so we should handle it.
+  expect_equal(sasParseAssignments('"123" = "YES"'), list(YES="123"))
+  expect_equal(sasParseAssignments('"123-456" = "YES"'), list(YES="123-456"))
+  expect_equal(sasParseAssignments('123-456 = "YES"'), list(YES="123-456"))
+  #   expect_equal( 
+  #     sasParseAssignments('"999  " = "ONE" "777  "-"8881 " = "TWO"', stripWhiteSpace=F),
+  #     list(ONE="999  ", TWO='777  -8881 '))
+  
+  expect_equal(sasParseAssignments(
+    '"41511"-"41519", "4160 "-"4169 ","4179 " = "PULMCIRC" "5571 ","5579 ", "V434 "= "PERIVASC"'),
+    list(
+      PULMCIRC=c('41511-41519','4160-4169','4179'),
+      PERIVASC=c('5571','5579','V434')
+    )
+  )
+  
+})
