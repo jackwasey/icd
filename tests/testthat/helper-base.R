97c0ff914eca0066b837310352ff06ce01fa0141
Jack Wasey <jack@jackwasey.com>
2015-01-12 09:49:36 -0500

    do.slow default to TRUE

--- tests/testthat/helper-base.R
+++ tests/testthat/helper-base.R
@@ -4,6 +4,8 @@
 #
 # i want test coverage on travis, and can wait for the remote builds. NOT_CRAN
 # appears to be set only by devtools::check()
+
+if (!exists("do.slow")) do.slow = TRUE
 do.slow %<>% or(identical(Sys.getenv("TRAVIS"), "true"))
 
 if (do.slow) message("Doing slow tests") else message("Skipping slow tests")
