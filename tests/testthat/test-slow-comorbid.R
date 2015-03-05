b71dcfbe850c215c978bdff2c1d964242f69e8f1
Jack Wasey <jack@jackwasey.com>
2015-03-05 06:04:26 -0500

    consolidate fast and slow tests (now they're all fast)

--- tests/testthat/test-slow-comorbid.R
+++ tests/testthat/test-slow-comorbid.R
@@ -518,3 +518,83 @@ test_that("diff comorbid works", {
   capture.output(resq <- icd9DiffComorbid(quanElixComorbid, quanDeyoComorbid, show = TRUE))
 
 })
+
+twoPts <- data.frame(visitId = c("v01", "v01", "v02", "v02"),
+                     icd9 = c("040", "000", "100", "000"), stringsAsFactors = FALSE)
+twoMap <- data.frame("malady" = c("100", "2000"), "ailment" = c("003", "040"), stringsAsFactors = FALSE)
+twoPtsFac <- data.frame(visitId = c("v01", "v01", "v02", "v02"), icd9 = c("040", "000", "100", "000"), stringsAsFactors = TRUE)
+twoMapFac <- data.frame("malady" = c("100", "2000"), "ailment" = c("003", "040"), stringsAsFactors = TRUE)
+
+test_that("comorbid quick test", {
+  testres <- icd9Comorbid(twoPts, twoMap, return.df = TRUE)
+  trueres <- data.frame("visitId" = c("v01", "v02"),
+                        "malady" = c(FALSE, TRUE),
+                        "ailment" = c(TRUE, FALSE),
+                        stringsAsFactors = FALSE)
+  expect_equal(testres, trueres)
+
+  testmat <- icd9Comorbid(twoPts, twoMap, return.df = FALSE)
+  truemat <- matrix(c(FALSE, TRUE, TRUE, FALSE), nrow=2,
+                    dimnames = list(c("v01", "v02"), c("malady", "ailment")))
+  expect_equal(testmat, truemat)
+
+  testresfac <- icd9Comorbid(twoPtsFac, twoMapFac, return.df = TRUE)
+  trueresfac <- data.frame("visitId" = c("v01", "v02"),
+                           "malady" = c(FALSE, TRUE),
+                           "ailment" = c(TRUE, FALSE),
+                           stringsAsFactors = TRUE)
+  expect_equal(testresfac, trueresfac)
+  expect_equal(icd9Comorbid(twoPtsFac, twoMapFac), truemat)
+
+})
+
+test_that("control params don't affect result of comorbid calc", {
+  pts <- randomPatients(101, 13)
+  pts$visitId <- asCharacterNoWarn(pts$visitId)
+  pts$icd9 <- as.factor(pts$icd9)
+  upts <- length(unique(pts$visitId))
+  ac <-  lapply(ahrqComorbid, function(x) {
+    f <- factor(x, levels(pts[["icd9"]]))
+    f[!is.na(f)]
+  })
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 1, chunkSize=32),
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=32)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 2, chunkSize=32),
+    icd9ComorbidShortCpp(pts, ac, threads = 5, chunkSize=32)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=1),
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=32)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 4, chunkSize=upts-1),
+    icd9ComorbidShortCpp(pts, ac, threads = 4, chunkSize=upts)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 4, chunkSize=upts-1),
+    icd9ComorbidShortCpp(pts, ac, threads = 4, chunkSize=upts+1)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 4, chunkSize=upts+1),
+    icd9ComorbidShortCpp(pts, ac, threads = 4, chunkSize=upts)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=upts-2, ompChunkSize = 1),
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=upts+2, ompChunkSize = 1)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=upts-2, ompChunkSize = 11),
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=upts+2, ompChunkSize = 11)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=upts, ompChunkSize = 1),
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=upts, ompChunkSize = 11)
+  )
+  expect_identical(
+    icd9ComorbidShortCpp(pts, ac),
+    icd9ComorbidShortCpp(pts, ac, threads = 3, chunkSize=3, ompChunkSize = 5) # primes < unique visits
+  )
+})
--- tests/testthat/test-slow-score.R
+++ tests/testthat/test-slow-score.R
@@ -150,3 +150,14 @@ test_that("count icd9 codes", {
                c("j" = 0, "k" = 0))
 
 })
+
+test_that("count wide directly (old func) same as reshape count", {
+
+  widedf <- data.frame(visitId = c("a", "b", "c"),
+                       icd9_01 = c("441", "4424", "441"),
+                       icd9_02 = c(NA, "443", NA))
+
+  # we don't get names back for the vector for 'long'
+  expect_equivalent(icd9CountWide(widedf),
+                    icd9Count(icd9WideToLong(widedf)))
+})
