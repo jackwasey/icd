3092e4714903591d4429665b41009e6fb759dd4e
Jack Wasey <jack@jackwasey.com>
2015-03-10 11:46:17 -0400

    added script to run docker undefined behavior sanitizer with XML deps

---
+++ inst/scripts/ubsan-docker.sh
@@ -0,0 +1,6 @@
+#!/bin/sh
+# e.g.
+# R CMD build icd9
+# ./ubsan-docker icd9_1.1.tar.gz
+docker run --rm -ti -v $(pwd):/mnt  rocker/r-devel-ubsan-clang sh -c "apt-get update && apt-get install --yes --fix-missing r-cran-xml libxml2-dev && check.r --setwd /mnt --install-deps $1"
+
