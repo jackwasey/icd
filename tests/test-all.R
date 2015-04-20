b249e025a24ea87da6f45a73524d2b5ae3665d42
Jack O. Wasey <jack@jackwasey.com>
2015-04-20 13:52:24 -0400

    use new check reporter

--- tests/test-all.R
+++ tests/test-all.R
@@ -25,4 +25,4 @@ if (identical(Sys.getenv("COVR"), "true")) {
   do_slow_tests <- FALSE
   do_online_tests <- FALSE
 }
-test_check("icd9", reporter = MultiReporter(reporters = list(SummaryReporter(), StopReporter())))
+test_check("icd9", reporter = MultiReporter(reporters = list(SummaryReporter(), CheckReporter())))
