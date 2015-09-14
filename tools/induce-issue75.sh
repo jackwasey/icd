5f228fab0effe0001ae17cac01b027e32e61882d
Jack Wasey <jack@jackwasey.com>
2015-09-14 05:28:29 -0400

    better bash scripts for stressing error 75

--- tools/induce-issue75.sh
+++ tools/induce-issue75.sh
@@ -1,11 +1,16 @@
-#!/bin/sh
+#!/bin/bash
 
 # run a thousand identical icd9 codes
-Rscript -e 'devtools::clean_dll(); devtools::load_all(); mydf <- data.frame(visitId = rep("a", 1000), icd9 = rep("441", 1000)); cmb <- icd9ComorbidQuanDeyo(mydf, isShort = FALSE, applyHierarchy = TRUE)'
+git pull && Rscript -e 'devtools::clean_dll(); devtools::load_all(); mydf <- data.frame(visitId = rep("a", 1000), icd9 = rep("441", 1000)); cmb <- icd9ComorbidQuanDeyo(mydf, isShort = FALSE, applyHierarchy = TRUE)'
 
 # run a single icd9 code
-Rscript -e 'devtools::clean_dll(); devtools::load_all(); mydf <- data.frame(visitId = "a", icd9 = "441"); cmb <- icd9ComorbidQuanDeyo(mydf, isShort = FALSE, applyHierarchy = TRUE)'
+git pull && Rscript -e 'devtools::clean_dll(); devtools::load_all(); mydf <- data.frame(visitId = "a", icd9 = "441"); cmb <- icd9ComorbidQuanDeyo(mydf, isShort = FALSE, applyHierarchy = TRUE)'
 
-# 
-R CMD build --no-build-vignettes && R CMD INSTALL `ls -tr | grep gz$ | tail -1` && Rscript -e 'library(icd9); mydf <- data.frame(visitId = rep("a", 1000), icd9 = rep("441", 1000)); cmb <- icd9ComorbidQuanDeyo(mydf, isShort = FALSE, applyHierarchy = TRUE)'
-
+# pull, build and stress, without docs for speed and to avoid premature errors
+#if [ -d "icd9" ]; then
+#  cd icd9
+#fi
+git --git-dir=icd9/.git pull
+R CMD build --no-build-vignettes
+R CMD INSTALL --no-docs --install-tests `ls -tr | grep icd9.*gz$ | tail -1`
+Rscript -e 'library(icd9); x<-data.frame(visitId = "a", icd9 = "441"); icd9ComorbidQuanDeyo(x, isShort = F, applyHierarchy = T)'
