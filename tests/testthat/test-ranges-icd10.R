3645ce4736dd909a80c5af4cd0df17fc42af3119
Jack Wasey <jack@jackwasey.com>
2015-11-09 08:23:02 -0500

    older range tests

--- tests/testthat/test-ranges-icd10.R
+++ tests/testthat/test-ranges-icd10.R
@@ -16,3 +16,16 @@
 # along with icd9. If not, see <http:#www.gnu.org/licenses/>.
 
 context("icd10 ranges")
+
+test_that("top level ICD-10 ranges between different alphabetic groups are not valid", {
+  # is this valid? injury codes, for example, go through S and T.
+  expect_error(icd10ExpandRange("A10", "B20"))
+  expect_error(icd10ExpandRange("C99", "B00"))
+  expect_error(icd10ExpandRange("G99.x", "H01"))
+  expect_error(icd10ExpandRange("G99", "H01.x"))
+  expect_error(icd10ExpandRange("G99.x", "H01.x"))
+})
+
+test_that("top level ICD-10 ranges of single code are expanded to real codes", {
+  expect_equals(icd10ExpandRange("J11", "J11", onlyReal = TRUE), )
+})
