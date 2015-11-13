f099107a5e666bd82eeac412526dd9db74312c54
Jack Wasey <jack@jackwasey.com>
2015-11-13 12:35:34 -0500

    scraping down to leaves, but phantomjs hanging

--- R/parse-icd10.R
+++ R/parse-icd10.R
@@ -59,7 +59,6 @@ scrape_icd10_who <- function(sleep_secs = 3) {
   requireNamespace("magrittr")
   requireNamespace("xml2")
   requireNamespace("rvest")
-  url <- "http://apps.who.int/classifications/icd10/browse/2016/en"
 
   remDr <- RSelenium::remoteDriver(browserName = "phantomjs")
 
@@ -78,56 +77,59 @@ scrape_icd10_who <- function(sleep_secs = 3) {
     phantomjs$stop()
   })
 
-  remDr$navigate(url)
 
-  # now what have we got?
-  main_index_src <- remDr$getPageSource()
-  main_index <- xml2::read_html(main_index_src[[1]])
-  writeLines(main_index_src[[1]], "temp.html")
+  if (FALSE)   {
+    url <- "http://apps.who.int/classifications/icd10/browse/2016/en"
+    remDr$navigate(url)
 
-  # show all a tags:
-  print(xml2::xml_find_all(main_index, "//a"))
-  # get all those a nodes: (?just at one level)
-  a_nodes <- rvest::html_nodes(main_index, "a")
+    # now what have we got?
+    main_index_src <- remDr$getPageSource()
+    main_index <- xml2::read_html(main_index_src[[1]])
+    writeLines(main_index_src[[1]], "temp.html")
 
-  # find just the anchors with the given class
-  a_nodes <- rvest::html_nodes(a_nodes, xpath = "//a[@class='ygtvlabel  ']") %>%
-    # and clean up:
-    rvest::html_text() %>%
-    trim() %>%
-    stringr::str_replace("^[^ ]+ ", "")
+    # show all a tags:
+    print(xml2::xml_find_all(main_index, "//a"))
+    # get all those a nodes: (?just at one level)
+    a_nodes <- rvest::html_nodes(main_index, "a")
 
-  # but we already knew the chapters, we need to click the links...
+    # find just the anchors with the given class
+    a_nodes <- rvest::html_nodes(a_nodes, xpath = "//a[@class='ygtvlabel  ']") %>%
+      # and clean up:
+      rvest::html_text() %>%
+      trim() %>%
+      stringr::str_replace("^[^ ]+ ", "")
 
-  # same using selenium/phantom:
-  chapterVII <- remDr$findElement(using = 'xpath', "//a[@data-id='VII']")
+    # but we already knew the chapters, we need to click the links...
 
-  # click a link:
-  # (press enter) chapterVII$sendKeysToElement(list("\uE007"))
-  print(remDr$getCurrentUrl())
+    # same using selenium/phantom:
+    chapterVII <- remDr$findElement(using = 'xpath', "//a[@data-id='VII']")
 
-  chapterVII$clickElement()
+    # click a link:
+    # (press enter) chapterVII$sendKeysToElement(list("\uE007"))
+    print(remDr$getCurrentUrl())
 
-  # did URL change?
-  print(remDr$getCurrentUrl())
+    chapterVII$clickElement()
 
-  # and see what we have now:
-  chapterVII_clicked <- remDr$getPageSource()[[1]]
-  writeLines(chapterVII_clicked[[1]], "chapter7.html")
+    # did URL change?
+    print(remDr$getCurrentUrl())
 
-  # yes, we now have things like:
-  # <li class="Blocklist1">
-  #   <a href="#/H15-H22" title="Disorders of sclera, cornea, iris and ciliary body" class="code">H15-H22</a>
-  #   <span class="label">Disorders of
-  # sclera, cornea, iris and ciliary body</span>
-  #   </li>
+    # and see what we have now:
+    chapterVII_clicked <- remDr$getPageSource()[[1]]
+    writeLines(chapterVII_clicked[[1]], "chapter7.html")
 
-
-  #Hfifteen <- remDr$findElements(using = "xpath", "//li[@class='Blocklist1']")
-  #print(Hfifteen[[1]]$getElementText()) # now we have a subshapter
-  #  "H00-H06\nDisorders of eyelid, lacrimal system and orbit"
+    # yes, we now have things like:
+    # <li class="Blocklist1">
+    #   <a href="#/H15-H22" title="Disorders of sclera, cornea, iris and ciliary body" class="code">H15-H22</a>
+    #   <span class="label">Disorders of
+    # sclera, cornea, iris and ciliary body</span>
+    #   </li>
 
 
+    #Hfifteen <- remDr$findElements(using = "xpath", "//li[@class='Blocklist1']")
+    #print(Hfifteen[[1]]$getElementText()) # now we have a subshapter
+    #  "H00-H06\nDisorders of eyelid, lacrimal system and orbit"
+
+  }
 
 
 
@@ -140,8 +142,8 @@ scrape_icd10_who <- function(sleep_secs = 3) {
   #     http://apps.who.int/classifications/icd10/browse/2016/en#/VII
   #     scrape sub-chapter names and ranges: e.g. H30-H36 Disorders of choroid and retina
 
-  chapter_urls <- paste0("http://apps.who.int/classifications/icd10/browse/2016/en#/",
-                         as.roman(1:21))
+  who_icd10_url_base <- "http://apps.who.int/classifications/icd10/browse/2016/en#/"
+  chapter_urls <- paste0(who_icd10_url_base, as.roman(1:21))
 
   sub_chapters <- list()
 
@@ -153,38 +155,95 @@ scrape_icd10_who <- function(sleep_secs = 3) {
     # chapter_xml <- xml2::read_html(chapter_html[[1]])
     sub_chapters_xml <- remDr$findElements(using = "xpath", "//li[@class='Blocklist1']")
     message("got sub_chapters")
-    sub_chapters <- lapply(
-      sub_chapters_xml,
-      function(x) strsplit(x$getElementText()[[1]], "\\n") %>%
-        magrittr::extract2(1) %>%
-        magrittr::extract(1) %>%
-        strsplit("-") %>%
-        magrittr::extract2(1) %>%
-        magrittr::set_names(c("start", "end"))
-    )
-    names(sub_chapters) <- lapply(sub_chapters_xml, function(x)
-      strsplit(x$getElementText()[[1]], "\\n")[[1]][2]
-    )
+    # sub_chapters <- lapply(
+    #   sub_chapters_xml,
+    #   function(x) strsplit(x$getElementText()[[1]], "\\n") %>%
+    #     magrittr::extract2(1) %>%
+    #     magrittr::extract(1) %>%
+    #     strsplit("-") %>%
+    #     magrittr::extract2(1) %>%
+    #     magrittr::set_names(c("start", "end"))
+    # )
+    #names(sub_chapters) <- lapply(sub_chapters_xml, function(x)
+    #  strsplit(x$getElementText()[[1]], "\\n")[[1]][2]
+    #)
+
+    # use for loop for debugging:
+    sub_chapters <- list()
+    for (x in sub_chapters_xml) {
+      Sys.sleep(sleep_secs)
+      sub_chapter_text <- x$getElementText()[[1]]
+      message("working on a new sub chapter ", sub_chapter_text)
+      # if (sub_chapter_text == "A92-A99\nArthropod-borne viral fevers and viral haemorrhagic fevers") browser()
+      split_one <- strsplit(sub_chapter_text, "\\n")
+      range_only <- split_one[[1]][1]
+      name_only <- split_one[[1]][2]
+      split_range <- strsplit(range_only, "-")
+
+      sub_chapters[[name_only]] <- c(start = split_range[[1]][1],
+                                     end = split_range[[1]][2])
+
+    }
+    message("completed sub chapters")
+
 
     # next, look at individual heading and leaf (billing) codes
+    # this can be accomplished using constructed URLs, also, e.g.:
+    # http://apps.who.int/classifications/icd10/browse/2016/en#/A92-A99
+
+
+    # now in a new loop, we can generate the drilled down URLs from the subchapter
+    # ranges without mucking around by 'clicking' on links
+    #  e.g. http://apps.who.int/classifications/icd10/browse/2016/en#/H40-H42
+    #   there are "Category 1" elements which are equivalent to 'major' types, e.g. H40 Glaucoma
+    # <h4>
+    # <a name="H40" id="H40" class="code">H40</a>
+    # <span class="label">Glaucoma</span>
+    # </h4>
+
+    #   and "category 2" elements which are the leaf nodes. e.g. H40.3 Glaucoma secondary to eye trauma
+    # <h5>
+    # <a name="H40.0" id="H40.0" class="code">H40.0</a>
+    # <span class="label">Glaucoma suspect</span>
+    # </h5>
+
+
+    majors <- list()
+    leaves <- list()
+    for (sub_chapter in sub_chapters) {
+      sub_chapter_url <- paste0(who_icd10_url_base, sub_chapter$start, "-", sub_chapter$end)
+      message(sub_chapter_url)
+      remDr$navigate(sub_chapter_url)
+      majors_code_xml <- remDr$findElements(using = "xpath", "//div[@class='Category1']//a")
+      majors_desc_xml <- remDr$findElements(using = "xpath", "//div[@class='Category1']//span")
+      stopifnot(length(majors_code_xml) == length(majors_desc_xml))
+
+      #majors <- data.frame(major=c(), desc=c())
+      for (i in length(majors_xml)) {
+        major_code <- majors_code_xml[[i]]$getElementText() %>% unlist
+        major_desc <- majors_desc_xml[[i]]$getElementText() %>% unlist
+        majors[[major_desc]] <- major_code
+      }
+
+      leaves_code_xml <- remDr$findElements(using = "xpath", "//div[@class='Category2']//a")
+      leaves_desc_xml <- remDr$findElements(using = "xpath", "//div[@class='Category2']//span")
+      stopifnot(length(leaves_code_xml) == length(leaves_desc_xml))
+
+      for (i in length(leaves_code_xml)) {
+        leaf_code <- leaves_code_xml[[i]]$getElementText() %>% unlist
+        leaf_desc <- leaves_desc_xml[[i]]$getElementText() %>% unlist
+        leaves[[leaf_desc]] <- leaf_code
+      }
+
+      # someday, add the exclusion rubric to the data structure for detailed validation
+
+      print(head(majors))
+      print(head(leaves))
+    }
+
+    # todo sort so that majors come immediately before all of their leaf children
 
   }
-
-  # now in a new loop, we can generate the drilled down URLs from the subchapter
-  # ranges without mucking around by 'clicking' on links
-  #  e.g. http://apps.who.int/classifications/icd10/browse/2016/en#/H40-H42
-  #   there are "Category 1" elements which are equivalent to 'major' types, e.g. H40 Glaucoma
-  # <h4>
-  # <a name="H40" id="H40" class="code">H40</a>
-  # <span class="label">Glaucoma</span>
-  # </h4>
-
-  #   and "category 2" elements which are the leaf nodes. e.g. H40.3 Glaucoma secondary to eye trauma
-  # <h5>
-  # <a name="H40.0" id="H40.0" class="code">H40.0</a>
-  # <span class="label">Glaucoma suspect</span>
-  # </h5>
-
 
 
 
