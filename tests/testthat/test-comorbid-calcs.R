06ded39b1e4ebd68a0b1b695411485556e3a3584
Jack Wasey <jack@jackwasey.com>
2015-12-13 19:04:50 -0500

    tests and examples all pass again

--- tests/testthat/test-comorbid-calcs.R
+++ tests/testthat/test-comorbid-calcs.R
@@ -333,7 +333,7 @@ test_that("get Charlson/Deyo comorbidities for a single patient", {
 })
 
 test_that("if an ICD class is not specified for a data set, but is for a column therein, the correct method is dispatched", {
-  mydf <- data.frame(visitId = c("a", "b", "c"), icd9 = c("412.93", "441", "044.9"))
+  mydf <- icd9(data.frame(visitId = c("a", "b", "c"), icd9 = c("412.93", "441", "044.9")))
   expect_error(icd_comorbid_quan_elix(mydf, short_code = FALSE, hierarchy = TRUE, return_df = TRUE), NA)
   # TODO, expand this section
 })
