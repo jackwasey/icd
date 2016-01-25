cd8f6e9ef88cc1aacc66c024a10a13a07569d131
Jack Wasey <jack@jackwasey.com>
2016-01-25 10:57:47 -0500

    tweaked valgrind tools

--- tools/valgrind-one-test.sh
+++ tools/valgrind-one-test.sh
@@ -3,11 +3,11 @@ ICD9_HOME=$HOME/Documents/RProjects/icd9
 cd $ICD9_HOME
 #R --vanilla --slave -d "valgrind --tool=callgrind --simulate-cache=yes --instr-atstart=no -v" -e "library(devtools); library(testthat); load_all(); icd9ComorbidQuanDeyo(icd9RandomShort(1000))"
 
-${RCODE:="devnull <- icd9ComorbidQuanDeyo(icd9:::randomPatients(1000000))"}
+${RCODE:="library(testthat); devtools::load_all(); message('Testing: $1'); source('$ICD9_HOME/tests/testthat/helper-base.R'); test_file('$1')"}
 INSTR_ATSTART="no"
 VALGRIND_CMD="valgrind --tool=callgrind --simulate-cache=yes --instr-atstart=$INSTR_ATSTART --separate-threads=yes"
 
-R --vanilla --slave -d "$VALGRIND_CMD" -e "library(icd9); message(\"ready to valgrind\"); $RCODE"
+R --vanilla --slave -d "$VALGRIND_CMD" -e "message(\"ready to valgrind\"); $RCODE"
 
 
 # other useful options: --instr-atstart=no --separate-threads=yes
