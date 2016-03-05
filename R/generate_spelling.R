05340c9574e749af80b4c5dcd997d3642ab2867e
Jack Wasey <jack@jackwasey.com>
2016-03-05 18:18:10 -0500

    generate spelling as function, not run in namespace

--- R/generate_spelling.R
+++ R/generate_spelling.R
@@ -1,11 +1,9 @@
 # This is a bit painful, but defining known spellings of names and technical terms makes the aspell checks useful (and removes a NOTE in the R CMD check which at least some of the CRAN people use.
 
+generate_spelling <- function(out = ".aspell/words.pws") {
 proper_nouns <- c("Wasey", "Quan", "Walraven", "Deyo", "wmurphyrd", "Elixhauser")
 proper_nouns <- c(proper_nouns, paste(proper_nouns, "'s", sep = ""))
-
 acronyms <- c("POA", "ICD", "CRAN", "CMS", "AHRQ", "SAS")
-
 other <- c("comorbidity", "comorbidities", "comorbid", "unclassifiable")
-
-aspell_write_personal_dictionary_file(c(proper_nouns, acronyms, other), out = ".aspell/words.pws")
-
+aspell_write_personal_dictionary_file(c(proper_nouns, acronyms, other), out = out)
+}
