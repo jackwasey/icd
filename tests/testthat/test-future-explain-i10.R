402c405deae0800c29a83c1bbd80fd8bbde4e49d
Jack Wasey <jack@jackwasey.com>
2016-03-10 10:47:52 -0500

    nascent test for icd-10-cm explain

--- tests/testthat/test-future-explain-i10.R
+++ tests/testthat/test-future-explain-i10.R
@@ -15,18 +15,17 @@
 # You should have received a copy of the GNU General Public License
 # along with icd. If not, see <http:#www.gnu.org/licenses/>.
 
-context("generate defined child codes for ICD-10-CM")
+context("explain ICD-10-CM codes")
 
-expect_icd10cm_child_is_self <- function(x) {
-  for (i in x)
-    eval(bquote(expect_identical(icd_children(icd10cm(.(i))), icd10cm(.(i)))))
+expect_icd10cm_explain_one <- function(...) {
+  x <- list(...)
+  for (i in seq_along(x))
+    eval(bquote(expect_identical(icd_explain(icd10cm(.(names(x[i]))), x[[i]]))))
 }
 
-test_that("children of a leaf node returns itself", {
+test_that("explain leaf codes", {
 
-  expect_icd10cm_child_is_self(c("O9A119", "O9A53", "S00.00XA", "T3299", "P150", "P159", "Z9989", "Z950", "C7A098", "C7A8"))
-
-  rand_icd10cm <- generate_random_icd10cm_bill(50)
-  expect_icd10cm_child_is_self(rand_icd10cm)
-
+  expect_icd10cm_explain_one("G000" = "Hemophilus meningitis",
+                             "P960" = "Congenital renal failure"
+  )
 })
