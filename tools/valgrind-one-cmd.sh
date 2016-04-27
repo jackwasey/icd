ffe01e548fa38c7a200087ce382dc4b4a6aefad7
Jack Wasey <jack@jackwasey.com>
2016-04-27 12:53:21 -0400

    [ci skip] minor updates to tool scripts

--- tools/valgrind-one-cmd.sh
+++ tools/valgrind-one-cmd.sh
@@ -7,10 +7,10 @@ ICD_HOME=${ICD_HOME:-"$HOME/Documents/RProjects/icd"}
 cd $ICD_HOME
 
 
-${RCODE:="devnull <- icd_comorbid_quan_deyo(icd:::generate_random_unordered_pts(1000000))"}
+#${RCODE:-'invisible(icd_comorbid_quan_deyo(icd:::generate_random_unordered_pts(1000)))'}
 INSTR_ATSTART="yes"
 VALGRIND_CMD="valgrind --tool=callgrind --simulate-cache=yes --instr-atstart=$INSTR_ATSTART --separate-threads=yes"
 
-R --vanilla --slave -d "$VALGRIND_CMD" -e '$RCODE'
+R --vanilla --slave -d "$VALGRIND_CMD" -e "$RCODE"
 
 # then 'callgrind_control -i' or prograammatically start instrumentation
