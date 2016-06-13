fd11da2598425094658f3a514b402387d2eca93c
Ed Lee <eddylee@rocketmail.com>
2016-06-13 16:12:56 -0400

    Used <- instead of =

--- tests/testthat/test-current-explain.R
+++ tests/testthat/test-current-explain.R
@@ -19,7 +19,7 @@ context("explain ICD-9: code to human-readable")
 
 test_that("icd_explain of vectors returns same length vector? ", {
 
-  testcodes = c("362.5", "413.9", "414.01", "584.9", "357.2", "588.81", "414")
+  testcodes <- c("362.5", "413.9", "414.01", "584.9", "357.2", "588.81", "414")
 
   expect_equal(length (icd_explain(testcodes)), length(testcodes))
 
