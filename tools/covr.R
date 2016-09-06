ac2719de87c83c3c3bdb98a97ad8b4de81b58978
Jack O. Wasey <jack@jackwasey.com>
2016-09-06 18:00:15 -0400

    try travis again

--- tools/covr.R
+++ tools/covr.R
@@ -1,3 +1,8 @@
+#!/usr/bin/env Rscript
+
+# simple script to encapsulate common code between travis and wercker (or other)
+# builds.
+
 args = commandArgs(trailingOnly = TRUE)
 if (tolower(args[[1]]) == "true") {
 
@@ -15,6 +20,8 @@ if (tolower(args[[1]]) == "true") {
     'src/ranges_alt.cpp',
     'R/fuzz.R')
 
+  message("excluding from coverage: ", paste(line_exclusions))
+
   if (args[[2]] == "coveralls")
     covr::coveralls(quiet = TRUE, line_exclusions = line_exclusions)
   else if (args[[2]] == "codecov")
@@ -23,4 +30,7 @@ if (tolower(args[[1]]) == "true") {
     stop("Must use codecov or coveralls.")
 
   covr::package_coverage(type = "vignette", quiet = TRUE, line_exclusions = line_exclusions)
+} else {
+  message("Not running code coverage")
 }
+cat(0)
