e07ff919aa04dd2c7ef64a84f430e2bbf7a49504
Jack O. Wasey <jack@jackwasey.com>
2016-09-09 21:09:32 -0400

    valgrind update

--- tools/valgrind-misc.sh
+++ tools/valgrind-misc.sh
@@ -2,6 +2,8 @@
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
 IFS=$'\n\t'
+CALLGRIND="valgrind --tool=callgrind --simulate-cache=yes"
+PKG_ARG="--default-packages=icd"
 
 # R -d valgrind -e "library(icd); library(testthat); source('tests/testthat/helper-base.R');
 # R -d valgrind -e "library(icd); library(testthat); test_package('icd', reporter=VeryVerboseReporter())"
@@ -9,24 +11,22 @@ R -d "valgrind --leak-check=full --track-origins=yes --show-leak-kinds=all -v" -
 # R -d valgrind -e "icd::icd_comorbid_ahrq(data.frame(visit_id = c('a','a'), icd = c('39891','00321')), short_code = TRUE)"
 # R -d valgrind -e "source('tests/testthat/helper-base.R'); icd::icd_comorbid_ahrq(ahrqTestDat, isShort = T, abbrevNames = F, applyHierarchy = T)"
 
-R -d "valgrind --tool=callgrind --simulate-cache=yes" -e "library(icd); icd:::icd_bench_comorbid_parallel()"
+R -d "$CALLGRIND" -e "library(icd); icd:::icd_bench_comorbid_parallel()"
 # then run:
 # callgrind_annotate --auto=yes callgrind.out.#PID filename.cpp
 
 # test with real data, if available!
-R -d "valgrind --tool=callgrind --simulate-cache=yes" -e "library(icd); load('i9test.RData'); icd_comorbid_ahrq(i9test, visit_id = 'patcom', icd_name = 'i9diag')"
+R -d "$CALLGRIND" -e "library(icd); load('i9test.RData'); icd_comorbid_ahrq(i9test, visit_id = 'patcom', icd_name = 'i9diag')"
 
 # stress with fabricated data on parallel implementation
-R -d "valgrind --tool=callgrind --simulate-cache=yes" -e "library(icd); devnull<-icd:::sc(1000000)"
+R -d "$CALLGRIND" -e "library(icd); devnull<-icd:::sc(1000000)"
 
 # valgrind parallel code? yes, can use flag to split out threads.
 # another useful option I put in .valgrindrc is --instr-atstart=no, so setup runs fast, then use ifdefs to enable valgrind in the key code areas.
 
-R -d "valgrind --tool=callgrind --simulate-cache=yes" -e "library(icd); library(microbenchmark); bench_vary_n()"
+R -d "$CALLGRIND" -e "library(icd); library(microbenchmark); bench_vary_n()"
 
-# useful to install first...
-R CMD INSTALL icd && R -d "valgrind --tool=callgrind"  -e "library(icd); pts <- icd:::generate_random_ordered_pts(1000, 10); icd:::icd_long_to_wide(pts)"
-
+R CMD INSTALL ~/icd && Rscript $PKG_ARG -d "$CALLGRIND" -e "pts <- generate_random_ordered_pts(1000, 10); icd_long_to_wide(pts)"
 
 # other useful options: --instr-atstart=no
 # then callgrind_control -i (or prograammatically) start instrumentation
