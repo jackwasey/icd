aca47bd920c329519aca5d5eea684f53c0be4981
Jack Wasey <jack@jackwasey.com>
2017-04-30 21:06:03 -0400

    don't cran check in docker tools, focus on ubsan and leaks

--- tools/in_docker_check.sh
+++ tools/in_docker_check.sh
@@ -60,5 +60,5 @@ R_PKG_TAR_GZ=$(ls -t ${R_PKG_NAME}*.tar.gz | tail -1)
 # for all the flags
 # https://cran.r-project.org/doc/manuals/r-release/R-ints.html
 # not sure what happens if I us --as-cran and turn off a feature: seems like qpdf is still sought.
-_R_CHECK_DOC_SIZES_=false _R_CHECK_CRAN_INCOMING_=false _R_CHECK_FORCE_SUGGESTS_=false $R_CMD CMD check --as-cran $R_PKG_TAR_GZ
+_R_CHECK_DOC_SIZES_=false _R_CHECK_CRAN_INCOMING_=false _R_CHECK_FORCE_SUGGESTS_=false $R_CMD CMD check $R_PKG_TAR_GZ
 popd
