37d9afdb9b063284e2b8e2ad4cca8a4c781cdddb
Jack Wasey <jack@jackwasey.com>
2018-03-01 08:31:17 -0500

    add codecov to appveyor keep covr.R

--- tools/covr.R
+++ tools/covr.R
@@ -1,6 +1,7 @@
 #!/usr/bin/env Rscript
 
 # simple script to encapsulate coverage code for different build systems
+# Enables build matrix not to do code coverage for each permutation.
 
 # first argument is "true" or "false"
 # second argument is "codecov" or "coveralls"
@@ -10,28 +11,14 @@ if (tolower(args[[1]]) == "true") {
 
   options(warn = -1)
 
-  exclusions = list(
-    'inst/include/icd_RcppExports.h',
-    'R/children_alt.R',
-    'R/comorbid_alt.R',
-    'R/parse-icd10cm-xml.R',
-    'R/ranges_alt.R',
-    'R/sort_alt.R',
-    'src/convert_alt.cpp',
-    'src/manip_alt.cpp',
-    'src/ranges_alt.cpp',
-    'R/fuzz.R')
-
-  message("excluding from coverage: ", paste(exclusions))
-
   if (args[[2]] == "coveralls")
-    covr::coveralls(quiet = FALSE, exclusions = exclusions)
+    covr::coveralls(quiet = FALSE)
   else if (args[[2]] == "codecov")
-    covr::codecov(quiet = FALSE, exclusions = exclusions)
+    covr::codecov(quiet = FALSE)
   else
     stop("Must use codecov or coveralls.")
 
-  covr::package_coverage(type = "vignette", quiet = FALSE, exclusions = exclusions)
+  covr::package_coverage(type = "vignette", quiet = FALSE)
 } else {
   message("Not running code coverage")
 }
