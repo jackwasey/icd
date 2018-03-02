4ec71145bf4f9a2147cd511c3feb8edd901845ca
Jack O. Wasey <jack@jackwasey.com>
2018-03-02 00:08:43 -0500

    increase stack limit to allow compilation in docker container [ci skip]

--- tools/docker/in_docker_check.sh
+++ tools/docker/in_docker_check.sh
@@ -22,6 +22,18 @@ IFS=$'\n\t'
 # echo expanded commands
 set -x
 
+#https://stackoverflow.com/questions/14719349/error-c-stack-usage-is-too-close-to-the-limit#14719448
+# C stack limit problems hopefully fixed by this. Can also be set to "unlimited" Default is 8192k on my pc.
+# do this inside and outside the docker container
+
+old_ulimit=$(ulimit -s)
+ulimit -s unlimited
+
+function finish {
+        ulimit -s "$old_ulimit"
+}
+trap finish EXIT
+
 pushd /tmp
 
 echo 'debconf debconf/frontend select Noninteractive' | debconf-set-selections
