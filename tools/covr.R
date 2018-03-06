56b51b99b1ea51ad6cca26fd9a8e6ad3c11600ef
Jack Wasey <jack@jackwasey.com>
2018-03-05 20:22:11 -0500

    appveyor [ci skip] fedora docker option wip better covr.R parsing

--- tools/covr.R
+++ tools/covr.R
@@ -3,21 +3,30 @@
 # simple script to encapsulate coverage code for different build systems
 # Enables build matrix not to do code coverage for each permutation.
 
-# first argument is "true" or "false"
-# second argument is "codecov" or "coveralls"
-
 args = commandArgs(trailingOnly = TRUE)
+message("Running tools/covr.R with args: ", paste(args, collapse = ", "))
+
+if (length(args) != 2)
+  stop("first argument is 'true' or 'false' string, indicating whether to run coverage
+       second argument is package_coverage (for shiny output), coveralls or codecov.")
+
 if (tolower(args[[1]]) == "true") {
 
   # do give warnings as they arise
   options(warn = 1)
 
+  icd:::do_slow_tests()
+  # covr may start new process and thus ignore the R options set above. Try:
+  Sys.setenv(ICD_SLOW_TESTS = "true")
+
   if (args[[2]] == "coveralls")
     covr::coveralls(quiet = FALSE)
   else if (args[[2]] == "codecov")
     covr::codecov(quiet = FALSE)
+  else if (args[[2]] == "package_coverage")
+    covr::report(covr::package_coverage())
   else
-    stop("Must use codecov or coveralls.")
+    stop("Must use package_coverage, codecov or coveralls.")
 
   covr::package_coverage(type = "vignette", quiet = FALSE)
 } else {
