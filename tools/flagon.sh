fa3182b4d1c393be686859fd5baaee9725c210db
Jack Wasey <jack@jackwasey.com>
2018-03-12 12:45:16 -0400

    [ci skip] tools

--- tools/flagon.sh
+++ tools/flagon.sh
@@ -6,5 +6,5 @@ flag="${1:-ICD_DEBUG}"
 
 ICD_HOME=${ICD_HOME:-"$HOME/rprojects/icd"}
 
-sed -i '' "s@// *#define ${flag}\$@#define ${flag}@" "$ICD_HOME/src/local.h"
+sed -i'' "s@// *#define ${flag}\$@#define ${flag}@" "$ICD_HOME/src/local.h"
 
