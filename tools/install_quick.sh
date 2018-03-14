38a85fd35454771457ac9644a6b782d9a15b6d7f
Jack Wasey <jack@jackwasey.com>
2018-03-14 12:47:04 -0400

    version bump and minor tool fix

--- tools/install_quick.sh
+++ tools/install_quick.sh
@@ -6,5 +6,5 @@ IFS=$'\n\t'
 
 pushd "${ICD_HOME:-$HOME/rprojects}"
 R CMD build --no-build-vignettes --no-resave-data icd
-R CMD INSTALL --install-tests --no-docs --as-cran "$(ls -t /tmp/icd*.tar.gz | head -1)"
+R CMD INSTALL --install-tests --no-docs "$(ls -t /tmp/icd*.tar.gz | head -1)"
 popd
