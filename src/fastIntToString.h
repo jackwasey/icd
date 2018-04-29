e9d4fc3c03943863e7c62810f6cc0f1837f32ccb
Jack O. Wasey <jack@jackwasey.com>
2018-04-29 06:08:57 -0400

    remove more alternative code from main build (fastIntToString) [ci skip]

--- src/fastIntToString.h
+++ src/fastIntToString.h
@@ -2,8 +2,6 @@
 #define FASTINTTOSTRING_H_
 
 #include <Rcpp.h>
-using namespace Rcpp;
-std::vector<std::string> fastIntToStringStd(std::vector<int> x);
 Rcpp::CharacterVector fastIntToStringRcpp(Rcpp::IntegerVector x);
 
 #endif /* FASTINTTOSTRING_H_ */
---
+++ src/fastIntToString_alt_Std.cpp
@@ -0,0 +1,20 @@
+#include <Rcpp.h>
+
+//' @name fastIntToString
+//' @title Fast convert integer vector to character vector
+//' @description Fast conversion from integer vector to character vector using C++
+//' @param x vector of integers
+//' @param bufferSize int if any input strings are longer than this number
+//'   (default 16) there will be memory errors. No checks done for speed.
+//' @keywords internal
+// [[Rcpp::export]]
+std::vector<std::string> fastIntToString_alt_Std(std::vector<int> x) {
+  std::vector<std::string>::size_type len = x.size();
+  std::vector<std::string> out(len);
+  char buffer[64];
+  for (std::vector<double>::size_type i = 0; i != len; ++i) {
+    sprintf(buffer, "%u", x[i]);
+    out[i] = buffer;
+  }
+  return out;
+}
