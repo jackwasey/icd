2ddf9a9704e4bd2548b639de288a23dd3d4945d7
Jack Wasey <jack@jackwasey.com>
2018-05-08 15:44:46 -0400

    docker and vignette typo [ci skip]

--- tools/docker/in_docker_get_icd.sh
+++ tools/docker/in_docker_get_icd.sh
@@ -20,7 +20,7 @@ set -euo pipefail
 IFS=$'\n\t'
 
 # or download zip from: https://github.com/jackwasey/icd/archive/master.zip
-echo "Cloning '${GIT_BRANCH:-omp-taskloop}' branch from '${GIT_URL:-https://github.com/jackwasey/icd.git}'"
+echo "Downloading/cloning '${GIT_BRANCH:-omp-taskloop}' branch from '${GIT_URL:-https://github.com/jackwasey/icd.git}'"
 #git clone --depth=1 -b $GIT_BRANCH $GIT_URL
 echo "Using github repo name '${GITHUB_REPO:-icd}'"
 wget "https://github.com/jackwasey/$GITHUB_REPO/archive/$GIT_BRANCH.zip"
@@ -29,12 +29,17 @@ tempdest=$(mktemp -d)
 unzip -d "$tempdest" "$GIT_BRANCH.zip"
 #zipdir=$(ls -r "$GITHUB_REPO")
 zipdir=("$tempdest"/*)
-mkidr -p "$GITHUB_REPO"
-mv "$zipdir"/* "$GITHUB_REPO"
-rm -rf "$tempdest"
+mkdir -p "$GITHUB_REPO"
+ls "$zipdir"
+ls "$GITHUB_REPO"
+ls
+mv "$zipdir" "$GITHUB_REPO"
+rmdir "$tempdest"
 
+
+ls "$GITHUB_REPO"
 # the auto-generated Rcpp code is always changing order, if not content. Rstudio generates automatically, but we have to do manually here:
 pushd "$GITHUB_REPO"
-"${R_CMD}script" -e 'Rcpp::compileAttributes()'
+# "${R_CMD}script" -e 'Rcpp::compileAttributes()'
 popd
 
