4b0e030d5fad0cc1e89b6195f0cbaa4049306a7d
Jack O. Wasey <jack@jackwasey.com>
2018-06-07 17:22:33 -0400

    side-by-side

--- benchmarks/bench-pccc.R
+++ benchmarks/bench-pccc.R
@@ -4,36 +4,34 @@
 # R --vanilla -d "valgrind --tool=callgrind --instr-atstart=no"
 #
 library(icd)
+library(dplyr)
 library(magrittr)
 library(R.cache)
 library(pccc)
-# try with and without ICD codes as a factor: need strings either way, but a
-# factor we should not be converting to string and back, just work on the factor
-# levels.
-
 do_icd9 <- TRUE
-divide <- 1L
-
 # simulating NEDS (icd-9) db for size (also need to simulate width, which is 15+4 columns!)
 n <- 28584301L
-n <- n %/% divide
-codes <- if (do_icd9) unclass(icd:::as_char_no_warn(icd.data::icd9cm_hierarchy$code)) else unclass(icd:::as_char_no_warn(icd.data::icd10cm2016$code))
+n <- n / 10
+codes <- if (do_icd9) {
+  unclass(icd:::as_char_no_warn(icd.data::icd9cm_hierarchy$code))
+} else {
+  unclass(icd:::as_char_no_warn(icd.data::icd10cm2016$code))
+}
 set.seed(1441)
 dat <- data.frame(id = as.character(n + seq(n)),
                   icd_code = sample(codes, n, replace = TRUE),
                   stringsAsFactors = TRUE)
 #system.time(pccc_dx <- comorbid_pccc_dx(dat))
-if (FALSE) {
+if (FALSE)
   profvis::profvis(pccc_dx <- comorbid_pccc_dx(dat))
-}
-
 # generate wide data with realistic number of codes per patient:
-
 pts_per_code_pos <- as.integer(n / (1:20) ^ 4)
-key = list(paste0("bench-pccc-div", divide, ifelse(do_icd9, "-icd9", "-icd10")))
-dat_wide <- R.cache::loadCache(key)
-if (is.null(dat_wide) || nrow(dat_wide) != n) {
-  dat_wide <- data.frame(id = dat$id,
+key_factors = list(paste0("bench-pccc-factors", do_icd9, n))
+key_str = list(paste0("bench-pccc-wide", do_icd9, n))
+dat_wide_factors <- R.cache::loadCache(key_factors)
+dat_wide_str <- R.cache::loadCache(key_str)
+if (is.null(dat_wide_factors) || nrow(dat_wide_factors) != n) {
+  dat_wide_factors <- data.frame(id = dat$id,
                          dx01 = dat$icd_code,
                          stringsAsFactors = TRUE)
   for (dx in seq(2L, 20L)) {
@@ -43,70 +41,81 @@ if (is.null(dat_wide) || nrow(dat_wide) != n) {
     f <- as.integer(sample(c(1L + seq_len(len), rep(1L, n - len))))
     attr(f, "levels") <- c(NA, sample(codes, len, replace = TRUE))
     attr(f, "class") <- "factor"
-    dat_wide[[paste0("dx", dx_str)]] <- f
+    dat_wide_factors[[paste0("dx", dx_str)]] <- f
   }
-  R.cache::saveCache(dat_wide, key)
+  R.cache::saveCache(dat_wide_factors, key_factors)
+  dat_wide_str <- dat_wide_factors[1]
+  for (i in seq_along(dat_wide_factors)) {
+    if (i == 1) next
+    dat_wide_str[names(dat_wide_factors)[i]] <-
+      as.character(dat_wide_factors[[i]])
+  }
 }
-
 if (FALSE)
   system.time(dat_long <- icd::wide_to_long(dat_wide))
-# ugh - very slow - 10 or 20 seconds on Mac on 1% of data
-
-# can we just calculate comorbidities for each column, then OR?
+2# ugh - very slow - 10 or 20 seconds on Mac on 1% of data instead, can we just
+# calculate comorbidities for each column, then OR? But icd has to work on the
+# generated ID column each time, which is a big time sink
 ptm <- proc.time()
 res <- lapply(
-  rev(names(dat_wide)[-1]),
+  rev(names(dat_wide_factors)[-1]),
   function(x) {
-    message("working on column: ", x);
+    message("working on column ", x);
     tm <- system.time(
       if (do_icd9)
-        icd9_comorbid_pccc_dx(dat_wide[c("id", x)], icd_name = x,
+        icd9_comorbid_pccc_dx(dat_wide_factors[c("id", x)], icd_name = x,
                               restore_id_order = FALSE, unique_ids = TRUE)
       else
-        icd10_comorbid_pccc_dx(dat_wide[c("id", x)], icd_name = x,
+        icd10_comorbid_pccc_dx(dat_wide_factors[c("id", x)], icd_name = x,
                                restore_id_order = FALSE, unique_ids = TRUE)
     )
     print(tm)
-    #gc(verbose = TRUE)
+    #gc(verbose = TRUE) # there is gc time between each column
   }
 )
 print(proc.time() - ptm)
 
+########
+# pccc #
+########
+ptm <- proc.time()
+pccc::ccc(dat_wide_str,
+          "id",
+          dx_cols = seq(2, ncol(dat_wide_str)),
+          icdv = 9)
+print(proc.time() - ptm)
+
+stop()
+
 # NEDS simulated data result: 14 mins on Mac (4 core, just 2.5Ghz 16G RAM) after
 # some simple optimization, with GC each step, 5m40s (vs 18 mins in JAMA letter)
 # without garbage collection, 10 seconds per col, more with more occupancy:
-# 4m40s, which is similar on xeon and mac
-
+# Down to 4m40s, which is similar on xeon and mac
 if (FALSE)
-  profvis::profvis(icd9_comorbid_pccc_dx(dat_wide[c("id", "dx10")],
+  profvis::profvis(icd9_comorbid_pccc_dx(dat_wide_factors[c("id", "dx10")],
                                          icd_name = "dx10",
                                          restore_id_order = FALSE,
                                          unique_ids = TRUE))
-# profiling shows that converting the ID column to string was a big time sink (repeated for each column!),
-# as was re-ordering the visit IDs, which could be skipped if we just want summary data.
-
-# try to get pccc working at all:
-m_tens <- 10^c(3, 4, 5)
+m_tens <- 10^c(seq_len(log10(n)))
 pccc_timings <- list()
-for (m in c(m_tens, n)) {
-  message("working on pccc:ccc_mat_rcpp with", m, "rows")
+for (m in unique(c(m_tens, n))) {
+  message("working on pccc:ccc_mat_rcpp with ", m, " rows")
   st <- system.time(
     pccc:::ccc_mat_rcpp(as.matrix(dat_wide[seq_len(m), -1]),
                         matrix("", nrow = m), 9) # gave up after about 10 minutes on first run.
   )
   pccc_timings[as.character(m)] <- as.list(st)$elapsed
-  message(" - took ", as.list(st)$elapsed, "seconds")
+  message(" - took ", as.list(st)$elapsed, " seconds")
 }
-# full simulated NEDS 21 mins on xeon 4 core, (likely similar on Mac)
-# twice the time for PCCC compared to icd on 72-core server (busy server)
-
+# full simulated NEDS 21 mins on xeon 4 core, (likely similar on Mac) vs twice
+# the time for PCCC compared to icd on 72-core server (busy server)
 if (FALSE)
-pccc::ccc(head(dat_wide),
-          "id",
-          dx_cols = seq(2, ncol(dat_wide)),
-          #dx_cols = tidyselect::vars_select(names(dat_wide), tidyselect::starts_with("dx")),
-          pc_cols = NULL,
-          icdv = 9)
+  pccc::ccc(head(dat_wide_str),
+            "id",
+            dx_cols = seq(2, ncol(dat_wide_str)),
+            #dx_cols = tidyselect::vars_select(names(dat_wide), tidyselect::starts_with("dx")),
+            # pc_cols,
+            icdv = 9)
 
 # finding unique values is slowest step: consider
 # https://github.com/jl2922/omp_hash_map or
