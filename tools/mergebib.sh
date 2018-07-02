de10cd60fdd463d673b4900f4e734c71cf9c718d
Jack O. Wasey <jack@jackwasey.com>
2018-07-02 06:02:27 -0400

    order bibliographic items alphabetically using bibtool for consistency between linux and mac, because zotero doesn't set sorted order the same on those platforms.

--- tools/mergebib.sh
+++ tools/mergebib.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
-cp vignettes/icd.bib vignettes/all.bib
-cat vignettes/other.bib >> vignettes/all.bib
-cat vignettes/gplv3.bib >> vignettes/all.bib
+#cp vignettes/icd.bib vignettes/all.bib
+#cat vignettes/other.bib >> vignettes/all.bib
+#cat vignettes/gplv3.bib >> vignettes/all.bib
+
+command -v bibtool >/dev/null 2>&1 || { echo >&2 "I require bibtool but it's not installed. Aborting."; exit 1; }
+
+bibtool -s gplv3.bib other.bib icd.bib > all.bib
 
