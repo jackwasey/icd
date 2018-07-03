92853c789af02d991430f89d7b90070203522a0e
Jack O. Wasey <jack@jackwasey.com>
2018-07-03 12:16:40 -0400

    updated references so JSS article compiles on Mac

--- tools/mergebib.sh
+++ tools/mergebib.sh
@@ -8,3 +8,21 @@ command -v bibtool >/dev/null 2>&1 || { echo >&2 "I require bibtool but it's not
 
 bibtool -s gplv3.bib other.bib icd.bib > all.bib
 
+# make sure there are no unescaped underscores in the URLs:
+if grep '[^\]_' all.bib | grep -v ^@
+then
+  echo "Underscores found in all.bib entries"
+
+  echo "attempting to fix"
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' all.bib
+  exit 1
+fi
+
+
