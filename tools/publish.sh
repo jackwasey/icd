f229b6ef21782f79a9329fa5313cc4b3d987e673
Jack Wasey <jack@jackwasey.com>
2019-05-15 15:29:01 -0400

    [ci skip] tool

--- tools/publish.sh
+++ tools/publish.sh
@@ -6,7 +6,7 @@ IFS=$'\n\t'
 
 set -x
 
-ICD_HOME=${ICD_HOME:-"$HOME/icd"}
+ICD_HOME=${ICD_HOME:-"$HOME/rprojects/icd"}
 GH_PAGES="$HOME/rprojects/icd-gh-pages"
 
 pushd "$ICD_HOME"
@@ -17,10 +17,12 @@ function finish {
 trap finish EXIT
 
 #Rscript --vanilla --default-packages=jwutil -e 'jwutil::reqinst("pkgdown"); pkgdown::build_site()'
-Rscript --vanilla -e 'jwutil::reqinst("pkgdown"); devtools::load_all(); pkgdown::build_site()'
+Rscript --vanilla -e 'jwutil::reqinst("pkgdown"); devtools::document(); pkgdown::build_site()'
 mkdir -p "$GH_PAGES"
-cp -r docs/* "$GH_PAGES"
-git -C "$GH_PAGES" status
-git -C "$GH_PAGES" commit -am "pkgdown rerun"
-git -C "$GH_PAGES" push origin gh-pages
+rsync -r --delete --filter='P .git' --filter='P .gitignore' "$ICD_HOME/docs/" "$GH_PAGES"
+popd
+pushd "$GH_PAGES"
+git status
+git commit -am "pkgdown rerun"
+git push origin gh-pages
 
