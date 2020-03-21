d14aa8d51d786ee6991c0bf97a3938da77165822
Jack Wasey <jack@jackwasey.com>
2020-03-21 11:14:15 -0400

    add sets of environment variable definitions for bash and makevars, and enable in travis

---
+++ tools/mk/travis-nowarn.mk
@@ -0,0 +1,7 @@
+#!/usr/bin/env make
+
+CXXFLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-pragma
+CXX11FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
+CXX14FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
+CXX17FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
+CXX20FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
