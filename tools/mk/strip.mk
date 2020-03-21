2f90fcce12a5215f6d415ce69c8b7a35210cde1c
Jack Wasey <jack@jackwasey.com>
2020-03-21 14:14:21 -0400

    updated configure.ac

---
+++ tools/mk/strip.mk
@@ -0,0 +1,9 @@
+#!/usr/bin/env make
+#Enable using configure --enable-icd-strip
+#With binary stripping, size is about 10% on Linux, 90% on Mac. CRAN Windows DLL is already small.
+strippedLib: $(SHLIB)
+	command -v strip >/dev/null && \
+	command -v uname >/dev/null && \
+	[[ $(uname -s) =~ Linux|Darwin ]] && \
+		strip -S $(SHLIB)
+.phony: strippedLib
