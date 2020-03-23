b7005c1c1af67e88b64c75e24de18f96a9884c2d
Jack Wasey <jack@jackwasey.com>
2020-03-23 18:34:34 -0400

    travis - fixed typo in makefile fragment for debugging

--- tools/mk/strip.mk
+++ tools/mk/strip.mk
@@ -5,8 +5,8 @@ strippedLib: $(SHLIB)
 	# so clumsy, but unambiguous, avoids weird multi level make vs bash escaping
 	command -v strip >/dev/null && \
 		command -v uname >/dev/null && \
-		[ x$(uname -s) = xLinux ]] && strip -S $(SHLIB)
+		[ x$(uname -s) = xLinux ] && strip -S $(SHLIB) || true
 	command -v strip >/dev/null && \
 		command -v uname >/dev/null && \
-		[ x$(uname -s) = xDarwin ]] && strip -S $(SHLIB)
+		[ x$(uname -s) = xDarwin ] && strip -S $(SHLIB) || true
 .phony: strippedLib
