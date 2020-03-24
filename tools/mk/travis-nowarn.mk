49277d81194aef78df78e10eddce42af456f8121
Jack Wasey <jack@jackwasey.com>
2020-03-24 15:04:45 -0400

    travis

--- tools/mk/travis-nowarn.mk
+++ tools/mk/travis-nowarn.mk
@@ -1,7 +1,8 @@
 #!/usr/bin/env make
 
-CXXFLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-pragma
-CXX11FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
-CXX14FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
-CXX17FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
-CXX20FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragma
+# belt and braces - should just need CXXFLAGS
+CXXFLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-pragmas
+CXX11FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragmas
+CXX14FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragmas
+CXX17FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragmas
+CXX20FLAGS+=-Wno-unknown-warning-option -Wno-unused-parameter -Wno-unused-variable -Wno-ignored-attributes -Wno-cast-function-type -Wno-unknown-warning-option -Wno-unknown-pragmas
