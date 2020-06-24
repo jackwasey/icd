3dc0b461e8e2c24fcdb6a97c8c985193bf88c033
Jack Wasey <jack@jackwasey.com>
2020-06-24 14:19:52 -0400

    fix bibtool script, fix latex formatting

--- tools/bibfix.sh
+++ tools/bibfix.sh
@@ -1,30 +1,46 @@
 #!/usr/bin/env bash
+#set -euo pipefail
+#set +o noglob
+#shopt -s globstar
+#shopt -s extglob
+set -x
 
-#all=all.bib
-
-command -v bibtool >/dev/null 2>&1 || { echo >&2 "I require bibtool but it's not installed. Aborting."; exit 1; }
+command -v bibtool >/dev/null 2>&1 || {
+	echo >&2 "I require bibtool but it's not installed. Aborting."
+	exit 1
+}
 
 if [ -d vignettes ]; then
-  pushd vignettes || { echo "cannot change to vignette directory"; exit 1; }
-  trap popd EXIT
+	cd vignettes || { echo "cannot change to vignette directory"; exit 1; }
+else
+	echo "No vignettes directory found. Assuming in it."
 fi
 
+declare -i bad=0
+
 # make sure there are no unescaped underscores in the URLs:
-for bib in icdjss.bib icdpkg.bib gplv3.bib other.bib
-do
-  if grep '[^\]_' "$bib" | grep -v ^@
-  then
-    echo "Underscores found in $bib entries. Fixing now."
-    for n in {0..12}
-    do
-        [[ -v VERBOSE ]] && "echo ${n}"
-      echo -n "."
-      perl -pi -e 's/(url.*[^\\])_(.*)/\1\\_\2/g' "$bib"
-    done
-  else
-    echo -e "\nNo underscores found in $bib" >&2
-  fi
+for bib in icd.bib icdjss.bib icdpkg.bib other.bib gplv3.bib
+do {
+	#[[ -f "${bib}" ]] || continue
+	if grep -q '[^\]_' "${bib}" | grep -v ^@
+	then
+		[[ -n ${VERBOSE:-} ]] && echo "Underscores found in ${bib} entries. Fixing now." >&2
+		bad=1
+		# repeat many times, as I couldn't do this easily with the PERL one-liner
+		sed -i '/[[:space:]]*url/s/\\?_/\\_/g' "${bib}"
+	else
+		[[ -n ${VERBOSE:-} ]] && echo "No underscores found in ${bib}" >&2
+	fi
+	# delete abstracts and other unwanted in article
+	for del in abstract eprinttype
+	do {
+		perl -p -000 -i -e \
+			's/[[:space:]]+'"${del}"'[[:space:]]+=[[:space:]]+\{[^}]*\},\n//g' \
+			"${bib}"; }
+		done
+		bibtool -s -i "${bib}" -o "${bib}" -- preserve.key.case=on
+		bibtool -s -i "${bib}" -o "${bib}" -- "delete.field = { abstract }"
+	}
 done
 
-#bibtool -s gplv3.bib other.bib icdjss.bib -- preserve.key.case=on > "$all"
-exit 0
+exit "${bad}"
