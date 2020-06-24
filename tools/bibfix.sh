30c43648b92e0e69e179def16fe041283e2c7984
Jack Wasey <jack@jackwasey.com>
2020-06-24 19:39:49 -0400

    JSS  methods section; bibtool call improved

--- tools/bibfix.sh
+++ tools/bibfix.sh
@@ -3,46 +3,38 @@
 #set +o noglob
 #shopt -s globstar
 #shopt -s extglob
-set -x
+#set -x
 
 command -v bibtool >/dev/null 2>&1 || {
-	echo >&2 "I require bibtool but it's not installed. Aborting."
-	exit 1
+    echo >&2 "I require bibtool but it's not installed. Aborting."
+    exit 1
 }
 
 if [ -d vignettes ]; then
-	cd vignettes || { echo "cannot change to vignette directory"; exit 1; }
+    cd vignettes || { echo "cannot change to vignette directory"; exit 1; }
 else
-	echo "No vignettes directory found. Assuming in it."
+    echo "No vignettes directory found. Assuming in it."
 fi
 
 declare -i bad=0
 
 # make sure there are no unescaped underscores in the URLs:
-for bib in icd.bib icdjss.bib icdpkg.bib other.bib gplv3.bib
+for bib in $(echo ./icd.bib ./icdjss.bib ./icdpkg.bib ./other.bib ./gplv3.bib ./*.bib | tr ' ' $'\n' | sort -u)
 do {
-	#[[ -f "${bib}" ]] || continue
-	if grep -q '[^\]_' "${bib}" | grep -v ^@
-	then
-		[[ -n ${VERBOSE:-} ]] && echo "Underscores found in ${bib} entries. Fixing now." >&2
-		bad=1
-		# repeat many times, as I couldn't do this easily with the PERL one-liner
-		sed -i '/[[:space:]]*url/s/\\?_/\\_/g' "${bib}"
-	else
-		[[ -n ${VERBOSE:-} ]] && echo "No underscores found in ${bib}" >&2
-	fi
-	# delete abstracts and other unwanted in article
-	for del in abstract eprinttype
-	do {
-		perl -p -000 -i -e \
-			's/[[:space:]]+'"${del}"'[[:space:]]+=[[:space:]]+\{[^}]*\},\n//g' \
-			"${bib}"; }
-		done
-		bibtool -s -i "${bib}" -o "${bib}" -r bibtool.rsc
-		#bibtool -s -i "${bib}" -o "${bib}" -- preserve.key.case=on
-		#bibtool -s -i "${bib}" -o "${bib}" -- "delete.field = { abstract }"
-		#bibtool -s -i "${bib}" -o "${bib}" -- "delete.field = { url }"
-	}
+    [[ -f "${bib}" ]] || continue
+    echo "Working on ${bib}"
+    if grep -q '[^\]_' "${bib}" | grep -v ^@
+    then
+        [[ -n ${VERBOSE:-} ]] && echo "Underscores found in ${bib} entries. Fixing now." >&2
+        bad=1
+        # repeat many times, as I couldn't do this easily with the PERL one-liner
+        sed -i '/[[:space:]]*url/s/\\?_/\\_/g' "${bib}"
+    else
+        [[ -n ${VERBOSE:-} ]] && echo "No underscores found in ${bib}" >&2
+    fi
+    # clean up bibtex output frmo zotero. JSS, uses natbib, which is BibTex-based, not BibLaTex
+    bibtool -s -i "${bib}" -o "${bib}" -r bibtool.rsc
+}
 done
 
 exit "${bad}"
--- vignettes/bibtool.rsc
+++ vignettes/bibtool.rsc
@@ -2,5 +2,10 @@
 
 preserve.keys=on
 preserve.key.case=on
+pass.comments=on
+
 delete.field={abstract}
 delete.field={url}
+delete.field={pmcid}
+delete.field={pmid}
+delete.field={eprinttype}
