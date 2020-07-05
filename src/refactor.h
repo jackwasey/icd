92b545d971b7e019a0f4a46805ddcf2b69bbab93
Jack Wasey <jack@jackwasey.com>
2020-07-05 16:47:37 -0400

    clean up a header

--- src/refactor.h
+++ src/refactor.h
@@ -1,18 +1,15 @@
 #ifndef REFACTOR_H_
 #define REFACTOR_H_
 
-#include "local.h"
-
-using namespace Rcpp;
-IntegerVector factorNoSort(const CharacterVector &x,
-                           const CharacterVector &levels,
+Rcpp::IntegerVector factorNoSort(const Rcpp::CharacterVector &x,
+                           const Rcpp::CharacterVector &levels,
                            const bool na_rm);
-Rcpp::IntegerVector refactor(const IntegerVector &x,
-                             const CV &new_levels,
+Rcpp::IntegerVector refactor(const Rcpp::IntegerVector &x,
+                             const Rcpp::CharacterVector &new_levels,
                              const bool exclude_na,
                              const bool validate = false);
-Rcpp::IntegerVector refactor_narm(const IntegerVector &x,
-                                  const CV &new_levels,
+Rcpp::IntegerVector refactor_narm(const Rcpp::IntegerVector &x,
+                                  const Rcpp::CharacterVector &new_levels,
                                   const bool validate = false);
-bool factorIsValid(const IntegerVector &f);
+bool factorIsValid(const Rcpp::IntegerVector &f);
 #endif /* REFACTOR_H_ */
