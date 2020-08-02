62ad4e784c6947a77e5c002b5b365e056c005900
Jack Wasey <jack@jackwasey.com>
2020-08-02 14:02:02 -0400

    format.sh shouldn't format bench deps [ci skip]

--- tools/format.sh
+++ tools/format.sh
@@ -1,15 +1,20 @@
 #!/usr/bin/env bash
+set -euo pipefail
 echo -n "Running clang-format using path: "
 command -v clang-format || { echo "clang-format not found" >&2; exit 1; }
 echo -n "clang-format version: "
 clang-format --version
 
 find "${1:-.}" \
+	-type d \( -name "benchmarks" \) -prune \
+	-o -type f -a \( \
 	-name '*.c' \
 	-o -name '*.h' \
-	-o \( -name '*.cpp' -a -not -name 'RcppExports.cpp' \) |
+	-o \( -name '*.cpp' -a -not -name 'RcppExports.cpp' \) \
+	\) -print |
 	while read -r f; do
-		xargs clang-format -i "$f";
+		echo "${f}"
+		clang-format -i "$f";
 	done
 
-Rscript -e 'styler::style_pkg(filetype = c("R", "Rprofile", "Rmd", "Rnw"))'
+	Rscript -e 'styler::style_pkg(filetype = c("R", "Rprofile", "Rmd", "Rnw"))'
