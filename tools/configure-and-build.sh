410fdb6f69c46660f237e5ff5d1da6b685ab3d3d
Jack Wasey <jack@jackwasey.com>
2020-08-04 10:43:30 -0400

    tooling and compiler warning fix
    a minor compiler warning was revealed once the morass of Eigen warnings was
    cleaned up

--- tools/configure-and-build.sh
+++ tools/configure-and-build.sh
@@ -1,36 +1,40 @@
 #!/usr/bin/env bash
 set -eu
 
-echo "This is a dirty way to build the library quickly, mainly
-just to test that configure script works. Uses -isystem instead
-of -I to include without diagnostics from Rcpp and RcppEigen"
+echo "This is a dirty way to build the library quickly, mainly just to test
+that configure script works. Uses -isystem instead of -I to include without
+diagnostics from Rcpp and RcppEigen. All the usual R toolchain environment is
+missing and manually set as needed just for the configure script. R SHLIB
+should set the normal R toolchain environment variables."
 
-echo "${COLOR_LIGHT_BLUE:-}Using icd in ${ICD_HOME?}${COLOR_NC:-}"
+IH="${PWD%/tools*}"
+echo "${COLOR_LIGHT_BLUE:-}Using icd in ${IH?}${COLOR_NC:-}"
+cd "${IH}" || { echo "cannot cd there!" >&2; exit 1; }
 declare -a warn_flags
-warn_flags=("")
+warn_flags=(-Wall -Wextra -pedantic)
 # this process still pickups up on ~/.R/Makevars
 
 (
-set -x
-R_HOME="$(R RHOME)"
-CXX_STD=CXX17
+{
+	set -x
+	R_HOME="$(R RHOME)"
+	#PKG_CXXFLAGS="-w $(Rscript -e 'Rcpp:::CxxFlags()') -I ${IH}/inst/include -I ${ICD_HOME}/inst/include/eigen-submodule"
+	PKG_CXXFLAGS="${warn_flags[*]} $(Rscript -e 'Rcpp:::CxxFlags()' | sed 's/-I/-isystem/') $(Rscript -e 'RcppEigen:::CxxFlags()' | sed 's/-I/-isystem/')"
+	#PKG_CXX11FLAGS="${PKG_CXXFLAGS}"
+	PKG_CFLAGS="${warn_flags[*]}"
+	# CXX="ccache g++"
+	# CC="ccache gcc"
+	CXX=$("${R_HOME}/bin/R" CMD config CXX)
+	CC=$("${R_HOME}/bin/R" CMD config CC)
+	export MAKEFLAGS=-j8
+	export PKG_CXXFLAGS PKG_CFLAGS CC CXX CXX_STD 
+	# also need CXX17FLAGS, or is this just for _additional_ flags for that
+	# compiler?
+	export PATH="/usr/lib/ccache:${PATH}"
 
-#PKG_CXXFLAGS="-w $(Rscript -e 'Rcpp:::CxxFlags()') -I ${ICD_HOME}/inst/include -I ${ICD_HOME}/inst/include/eigen-submodule"
-PKG_CXXFLAGS="${warn_flags[*]} $(Rscript -e 'Rcpp:::CxxFlags()' | sed 's/-I/-isystem/') $(Rscript -e 'RcppEigen:::CxxFlags()' | sed 's/-I/-isystem/')"
-#PKG_CXX11FLAGS="${PKG_CXXFLAGS}"
-PKG_CFLAGS="${warn_flags[*]}"
-# CXX="ccache g++"
-# CC="ccache gcc"
-CXX=$("${R_HOME}/bin/R" CMD config CXX)
-CC=$("${R_HOME}/bin/R" CMD config CC)
-export MAKEFLAGS=-j8
-export PKG_CXXFLAGS PKG_CFLAGS CC CXX CXX_STD 
-# also need CXX17FLAGS, or is this just for _additional_ flags for that
-# compiler?
-export PATH="/usr/lib/ccache:${PATH}"
-
-cd "${ICD_HOME?}"
-autoreconf
-./configure --enable-icd-makevars --enable-icd-shutup  --disable-icd-strip
-R CMD SHLIB --preclean --output=/tmp/icd.shlib src/*.cpp src/*.c
+	cd "${IH}" || { echo "cannot cd there!" >&2; exit 1; }
+	autoreconf -fv
+	./configure --enable-icd-makevars --enable-icd-shutup  --disable-icd-strip
+	R CMD SHLIB --preclean --output=/tmp/icd.shlib src/*.cpp src/*.c
+}
 )
